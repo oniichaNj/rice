dabdb1d40c51ac4e4804d15568c0776508d92f26
Dumb Thing <p@w>
2014-03-26 10:30:50 +0100

    new dwmstatus - mitm protection, cpu load, colored output when battery low

--- dwmstatus/dwmstatus.c
+++ dwmstatus/dwmstatus.c
@@ -12,6 +12,8 @@
 
 #include <X11/Xlib.h>
 
+#define MAX_ARP_CACHE_ENTRIES  1024
+
 char *tzberlin = "Europe/Berlin";
 
 static Display *dpy;
@@ -39,6 +41,30 @@ smprintf(char *fmt, ...)
 
 	return ret;
 }
+
+
+float getfreq(char *file) {
+  FILE *fd;
+  char *freq;
+  float ret;
+
+  freq = malloc(10);
+  fd = fopen(file, "r");
+  if(fd == NULL) {
+    fprintf(stderr, "Cannot open '%s' for reading.\n", file);
+    exit(1);
+  }
+
+  fgets(freq, 10, fd);
+  fclose(fd);
+
+  ret = atof(freq)/1000000;
+  free(freq);
+  return ret;
+}
+
+
+
 
 int getbattery() {
 	FILE *fd;
@@ -125,28 +151,85 @@ loadavg(void)
 	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
 }
 
+
+char *detect_arp_spoofing(void) {
+
+  FILE *fp;
+  int  i = 1, j;
+  char **ptr = NULL;
+  char buf[100], *mac[MAX_ARP_CACHE_ENTRIES];
+
+  if (!(fp = fopen("/proc/net/arp", "r"))) {
+    return NULL;
+  }
+
+  ptr = mac;
+
+  while (fgets(buf, sizeof(buf) - 1, fp)) {
+
+    /* ignore the first line. */
+    if (i == 1) { i = 0; continue; }
+
+    if ((*ptr = malloc(18)) == NULL) {
+      return NULL;
+    }
+
+    sscanf(buf, "%*s %*s %*s %s", *ptr);
+    ptr++;
+  }
+
+  /* end table with a 0. */
+  *ptr = 0;
+
+  fclose(fp);
+
+  for (i = 0; mac[i] != 0; i++)
+    for (j = i + 1; mac[j] != 0; j++)
+      if ((strcmp("00:00:00:00:00:00", mac[i]) != 0) &&
+          (strcmp(mac[i], mac[j]) == 0)) {
+
+        return "\033[48;5;196mDetected MITM!\033[0m";
+      }
+
+  return "MITMp enabled.";
+}
+
+
+
+
 int
 main(void)
 {
 	char *status;
 	char *avgs;
 	char *tmbln;
+	char *mitm;
 	int  batt;
-
+	float cpu0, cpu1;
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
 		return 1;
 	}
 
-	for (;;sleep(90)) {
+	for (;;sleep(5)) {
 		avgs = loadavg();
 		tmbln = mktimes("%a %d %b %H:%M %Z %Y", tzberlin);
 		batt = getbattery();
-		status = smprintf("Battery: %i % | Load: %s |  %s",
-				  batt, avgs, tmbln);
+		mitm = detect_arp_spoofing();
+                cpu0 = getfreq("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
+                cpu1 = getfreq("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq");
+	
+
+        	status = smprintf("Freq: (%0.2f|%0.2f) | Battery: %i % | %s | Load: %s | %s",
+				  cpu0, cpu1, batt, mitm, avgs, tmbln);
+
+		if (batt <= 20) {
+		  status = smprintf("Freq: (%0.2f|%0.2f) |\033[48;5;196m Battery: %i % \033[0m| %s | Load: %s | %s",
+				    cpu0, cpu1, batt, mitm, avgs, tmbln);
+		}
+
 		setstatus(status);
 		free(avgs);
-		//free(batt);
 		free(tmbln);
 		free(status);
 	}
